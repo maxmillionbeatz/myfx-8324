1bb8f39d9e742c38152c21407dc03ec3352d09fa
Mario Reder <mario.reder@pm.me>
2025-10-19 17:16:12 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -12,7 +12,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_ENABLE_ABSTRACT_WALLET": "false",
   "VITE_DISABLE_EVM_WALLETS": "false",
   "VITE_DISABLE_SOLANA_WALLETS": "false",
-  "VITE_WALLETCONNECT_PROJECT_ID": "",
+  "VITE_WALLETCONNECT_PROJECT_ID": "4208154807ae7292059b79bec13d9e70",
   "VITE_APP_NAME": "OKLONG",
   "VITE_APP_DESCRIPTION": "Decentralized exchange to trade crypto & stocks without KYC",
   "VITE_HAS_PRIMARY_LOGO": "false",
