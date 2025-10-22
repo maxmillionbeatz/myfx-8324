b67349ed439f16422bf7901f1ce98355d9c0cab9
Mario Reder <mario.reder@pm.me>
2025-10-22 15:36:26 +0200

    Setup DEX with workflow files and configuration

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
-  "VITE_ORDERLY_BROKER_ID": "demo",
+  "VITE_ORDERLY_BROKER_ID": "oklong",
   "VITE_ORDERLY_BROKER_NAME": "OKLONG",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
