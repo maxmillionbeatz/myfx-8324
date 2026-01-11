51b528169e8d5f78d42ce176c3ebdcde00b6bf06
Mario Reder <mario.reder@pm.me>
2026-01-11 15:29:21 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Decentralized exchange to trade crypto & stocks without KYC",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Portfolio,Rewards,Swap,Markets,Trading,Leaderboard,Vaults",
+  "VITE_ENABLED_MENUS": "Portfolio,Rewards,Swap,Markets,Trading,Leaderboard",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
   "VITE_ENABLE_CAMPAIGNS": "false",
